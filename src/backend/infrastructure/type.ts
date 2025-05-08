2cf1116de6eab4813b6c1c3873b139ba35aaccf9
toki0902 <toki.shigoto@gmail.com>
2025-05-09 03:30:42 +0900

    ⚡️ デプロイ用

--- src/backend/infrastructure/type.ts
+++ src/backend/infrastructure/type.ts
@@ -1,18 +1,3 @@
-// 動画の基本情報（snippet）
-type Snippet = {
-  publishedAt: string;
-  channelId: string;
-  title: string;
-  description: string;
-  thumbnails: Thumbnails;
-  channelTitle: string;
-  playlistId: string;
-  position: number;
-  resourceId: ResourceId;
-  videoOwnerChannelTitle: string;
-  videoOwnerChannelId: string;
-};
-
 type Thumbnails = {
   default: Thumbnail;
   medium?: Thumbnail;
@@ -31,11 +16,3 @@ type ResourceId = {
   kind: string;
   videoId: string;
 };
-
-interface VideoStatistics {
-  viewCount: string; // 視聴回数
-  likeCount: string; // 高評価数
-  dislikeCount?: string; // 低評価数（非表示設定の場合もあるためオプショナル）
-  favoriteCount: string; // お気に入り数（常に "0" で固定されている）
-  commentCount?: string; // コメント数（コメントが無効な場合もあるためオプショナル）
-}
